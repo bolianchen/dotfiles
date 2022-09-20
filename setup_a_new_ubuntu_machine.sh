ee93aab1d3b4f05e8d784ed0afd71ce320ca265e
Bryan <bolianchen@gmail.com>
2022-09-20 19:22:11 +0800

    Update the setup shell script for ubuntu and add vimrc

--- setup_a_new_ubuntu_machine.sh
+++ setup_a_new_ubuntu_machine.sh
@@ -17,17 +17,24 @@ echo "...done"
 
 # link tmux configs
 echo "Creating symlink to tmux-related files"
-ln -s $ $ROOT_DIR/ubuntu/tmux.ubuntu.conf ~/.tmux.conf
+rm ~/.tmux.conf
+ln -s $ROOT_DIR/ubuntu/tmux.ubuntu.conf ~/.tmux.conf
 
 # link vim configs
 echo "Creating symlink to vim-related files"
 mkdir -p ~/.vim/after/ftplugin
-ln -s $SHARED_DIR/vimrc ~/.vim/
-ln -s $SHARED_DIR/python.vim ~/.vim/after/ftplugin
-ln -s $SHARED_DIR/java.vim ~/.vim/after/ftplugin
-ln -s $SHARED_DIR/c.vim ~/.vim/after/ftplugin
+rm ~/.vim/vimrc
+rm ~/.vim/after/ftplugin/python.vim
+rm ~/.vim/after/ftplugin/java.vim
+rm ~/.vim/after/ftplugin/c.vim
+rm ~/.ideavimrc
+ln -s $SHARED_DIR/vimrc ~/.vim/vimrc
+ln -s $SHARED_DIR/python.vim ~/.vim/after/ftplugin/python.vim
+ln -s $SHARED_DIR/java.vim ~/.vim/after/ftplugin/java.vim
+ln -s $SHARED_DIR/c.vim ~/.vim/after/ftplugin/c.vim
 ln -s $SHARED_DIR/ideavimrc ~/.ideavimrc
 
 # link zsh configs
 echo "source ~/.dotfiles/shared/zshrc" >> ~/.zshrc 
+rm ~/.sh_aliases
 ln -s $SHARED_DIR/sh_aliases ~/.sh_aliases
